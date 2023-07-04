f6372bbbb76190f1c17ee562859f0fa67ce6c49a
GuoxiongXie <jake.g.xie@gmail.com>
2023-07-03 18:23:27 -0700

    program to increment count

--- RaceConditionVerification.py
+++ RaceConditionVerification.py
@@ -1,2 +1,23 @@
-from time import sleep
 from concurrent.futures import ThreadPoolExecutor
+from Counter import Counter
+
+def main() -> None:
+    for i in range(100):
+        increment_count()
+
+def increment_count():
+    counter = Counter()
+    print(f"The count is {counter.count}")
+    with ThreadPoolExecutor() as executor:
+        f1 = executor.submit(counter.increment)
+        f2 = executor.submit(counter.increment)
+
+        while True:  # simulate a do ... while loop in Python
+            if f1.done() and f2.done():
+                print(f"The resulted count is {counter.count}")
+                if counter.count % 2 != 0:
+                    print(f"Race condition detected!!!")
+                break
+
+if __name__ == "__main__":
+    main()
